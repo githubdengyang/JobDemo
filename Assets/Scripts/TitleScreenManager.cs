2e46866ec48c66976fe869a8a942e674ed315195
yang.deng <1341816304@qq.com>
2025-08-30 22:32:01 +0800

    EP. 10 SAVE GAME (Pt. 2)

--- Assets/Scripts/TitleScreenManager.cs
+++ Assets/Scripts/TitleScreenManager.cs
@@ -2,11 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
+using UnityEngine.UI;
 
 namespace SG
 {
     public class TitleScreenManager : MonoBehaviour
     {
+        [Header("Menus")]
+        [SerializeField] GameObject titleScreenMainMenu;
+        [SerializeField] GameObject titleScreenLoadMenu;
+
+        [Header("Buttons")]
+        [SerializeField] Button loadMenuReturnButton;
+        [SerializeField] Button mainMenuLoadGameButton;
         public void StartNetworkAsHost()
         {
             NetworkManager.Singleton.StartHost();
@@ -16,6 +24,30 @@ namespace SG
         {
             WorldSaveGameManager.instance.CreateNewGame();
             StartCoroutine(WorldSaveGameManager.instance.LoadWorldScene());
+        }
+
+        public void OpenLoadGameMenu()
+        {
+            //  CLOSE MAIN MENU
+            titleScreenMainMenu.SetActive(false);
+
+            //  OPEN LOAD MENU
+            titleScreenLoadMenu.SetActive(true);
+
+            //  SELECT THE RETURN BUTTON FIRST
+            loadMenuReturnButton.Select();
+        }
+
+        public void CloseLoadGameMenu()
+        {
+            //  CLOSE LOAD MENU
+            titleScreenLoadMenu.SetActive(false);
+
+            //  OPEN MAIN MENU
+            titleScreenMainMenu.SetActive(true);
+
+            //  SELECT THE LOAD BUTTON
+            mainMenuLoadGameButton.Select();
         }
     }
 }
